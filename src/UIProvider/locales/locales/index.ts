9720b8ae4a1c15467e1a62b3f6bf698f1b46aaec
Ian Huang <21ianh1@students.tas.tw>
2020-05-28 05:22:01 +0800

    feat: add zh

--- src/UIProvider/locales/locales/index.ts
+++ src/UIProvider/locales/locales/index.ts
@@ -1,5 +1,6 @@
 /* eslint-disable camelcase */
 import en_US from './en_US'
+import zh_TW from './zh_TW'
 
-export default { en_US }
+export default { en_US, zh_TW }
 export type _LocaleType = typeof en_US
