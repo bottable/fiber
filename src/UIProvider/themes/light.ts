799e81dc2ad1659eee2d631f5a678b19319b040c
Ian Huang <21ianh1@students.tas.tw>
2020-05-28 05:34:41 +0800

    feat: light color scheme

--- src/UIProvider/themes/light.ts
+++ src/UIProvider/themes/light.ts
@@ -1,1 +1,10 @@
-export default {}
+const colors = {
+  // status
+  success: '##40B3A1',
+  danger: '#e27d5f',
+  warning: '##E8A87C',
+  info: '##85CDC9',
+  error: '#e27d5f'
+}
+
+export default { colors }
