41910f53f876193ee63460fb388256aa1215203f
ian13456 <21ianh1@students.tas.tw>
2020-06-02 01:18:34 +0800

    feat: theme definition

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
 
 import { ExampleComponent } from 'fiber'
-import 'fiber/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text='Create React Library Example 😄' />
 }
 
 export default App
