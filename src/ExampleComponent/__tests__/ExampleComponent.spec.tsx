65a515e4988aa0548872f562e8e2b5ad998a7679
Ian Huang <21ianh1@students.tas.tw>
2020-06-05 23:45:27 +0800

    feat: setup jest setting system

--- src/ExampleComponent/__tests__/ExampleComponent.spec.tsx
+++ src/ExampleComponent/__tests__/ExampleComponent.spec.tsx
@@ -1,7 +1,19 @@
 import { ExampleComponent } from '..'
 
+import React from 'react'
+import { render } from 'test/utils'
+
 describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+  it('renders correctly', () => {
+    const exampleComponent = render(
+      <ExampleComponent
+        locale={{ welcome: 'locale overwrite' }}
+        mt='516px'
+        background='pink'
+        text='text'
+      />
+    )
+
+    expect(exampleComponent).toMatchSnapshot()
   })
 })
