549ec136aed31bb8fae5ccbf5e2cb0697d83f8e1
ian13456 <21ianh1@students.tas.tw>
2020-06-08 02:32:47 +0800

    fix: color theme missing properties

--- src/UIProvider/themes/index.ts
+++ src/UIProvider/themes/index.ts
@@ -1,6 +1,73 @@
-import * as colors from './colors'
+import {
+  blue,
+  green,
+  magenta,
+  neutral,
+  orange,
+  purple,
+  red,
+  teal,
+  yellow
+} from './colors'
+import * as misc from './misc'
 
-export * from './misc'
-export default colors
+// TODO: Write a better version of this...
 
-export type ThemeType = typeof colors.teal
+const blueTheme = {
+  ...blue,
+  ...misc
+}
+
+const greenTheme = {
+  ...green,
+  ...misc
+}
+
+const magentaTheme = {
+  ...magenta,
+  ...misc
+}
+
+const neutralTheme = {
+  ...neutral,
+  ...misc
+}
+
+const orangeTheme = {
+  ...orange,
+  ...misc
+}
+
+const purpleTheme = {
+  ...purple,
+  ...misc
+}
+
+const redTheme = {
+  ...red,
+  ...misc
+}
+
+const tealTheme = {
+  ...teal,
+  ...misc
+}
+
+const yellowTheme = {
+  ...yellow,
+  ...misc
+}
+
+export default {
+  blue: blueTheme,
+  green: greenTheme,
+  magenta: magentaTheme,
+  neutral: neutralTheme,
+  orange: orangeTheme,
+  purple: purpleTheme,
+  red: redTheme,
+  teal: tealTheme,
+  yellow: yellowTheme
+}
+
+export type ThemeType = typeof blueTheme
