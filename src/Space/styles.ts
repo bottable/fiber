fc037f0aaa0532a3c4b282f7ca112d1687bda79a
mrprokoala <zunigaruizb@gmail.com>
2020-06-08 00:16:29 +0800

    feat: only spacing between child components

--- src/Space/styles.ts
+++ src/Space/styles.ts
@@ -1,10 +1,11 @@
 import styled from 'styled-components'
 
 export const StyledBlock = styled.div`
+  display: inline-block;
   position: relative;
-  margin: 0.5em;
-  padding: 0.5em;
-  &:last-child() {
+  margin: 0;
+  padding: 0;
+  div:last-child {
     margin-right: 0;
   }
 `
