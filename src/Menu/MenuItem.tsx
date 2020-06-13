49aa5829cef805a8594d7baed021b80bb741d6f4
Alan Wu <ac2wu@edu.uwaterloo.ca>
2020-06-13 02:14:19 -0700

    feat: sub menu and menu item select

--- src/Menu/MenuItem.tsx
+++ src/Menu/MenuItem.tsx
@@ -2,39 +2,46 @@ import { StyleProps, styleComposition } from '../utils/styles'
 
 import { useMenu } from './useMenu'
 
-import styled from 'styled-components'
-import React, { FC } from 'react'
+import styled, { css } from 'styled-components'
+import React, { FC, useState } from 'react'
 
-export interface MenuItemProps extends StyleProps {}
+export interface MenuItemProps extends StyleProps { }
 
+const selectedItem = css`
+  color: white;
+  background: ${(props) => props.theme.colors.lightest};
+  border-right: solid 3.5px ${(props) => props.theme.colors.light};
+`
 const BlockMenuItem = styled.li<any>`
   ${styleComposition}
 
   display: block;
   padding: 8px 10px;
   float: none;
-  border-right: solid ${(props) => props.theme.colors.gray3} 0.5px;
   color: ${(props) => props.color};
   text-align: left;
   text-decoration: none;
 
   &:hover {
-    background: ${(props) => props.theme.colors.gray3};
+    color: ${(props) => props.theme.colors.light};
     cursor: pointer;
   }
+
+  ${(props) => props.selected ? selectedItem : null};
 `
 
-const InlineMenuItem = styled(BlockMenuItem)<any>`
+const InlineMenuItem = styled(BlockMenuItem) <any>`
   display: inline-block;
   float: left;
 `
 
 const MenuItem: FC<MenuItemProps> = ({ children, ...props }) => {
   const { inline } = useMenu()
+  const [selected, setSelected] = useState<boolean>(false)
 
   const Item = inline ? InlineMenuItem : BlockMenuItem
 
-  return <Item {...props}>{children}</Item>
+  return <Item {...props} selected={selected} onClick={() => setSelected(!selected)}>{children}</Item>
 }
 
 export { MenuItem }
