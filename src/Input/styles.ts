6f4ff2c5691e7f48bf2a3431f69c140d0557103a
balta-z-r <zunigaruizb@gmail.com>
2020-07-30 05:47:51 +0800

    feat: default style for input

--- src/Input/styles.ts
+++ src/Input/styles.ts
@@ -4,11 +4,14 @@ import { rem } from 'polished'
 
 export const BaseInput = styled.input<InputProps>`
   display: inline-flex;
-  padding: ${rem('5px')};
-  border: ${rem('1px')} solid #c4c4c4;
-  border-radius: ${rem('4px')};
+  padding: ${`${rem('4px')} ${rem('11px')}`};
+  border: ${rem('1px')} solid #d9d9d9;
+  border-radius: ${rem('2px')};
+  color: rgb(0, 0, 0, 0.6);
+  font-size: ${rem('14px')};
   &:focus,
   &:hover {
     border-color: ${({ theme }) => theme.colors.base};
+    outline: none;
   }
 `
