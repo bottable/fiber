ed6c328dbcc0d501ded7a84e8230152bce1c3e7e
balta-z-r <zunigaruizb@gmail.com>
2020-07-30 21:55:42 +0800

    feat: size

--- src/Input/Input.tsx
+++ src/Input/Input.tsx
@@ -1,18 +1,36 @@
 import { MergeElementProps } from '../utils'
 
-import { BaseInput } from './styles'
+import { SmallInput, MediumInput, LargeInput } from './styles'
 
 import React from 'react'
 
 export type InputProps = MergeElementProps<
   'input',
   {
-    variant?: String
+    size?: 'sm' | 'md' | 'lg'
   }
 >
 
-const Input = React.forwardRef((props: InputProps, ref: any) => {
-  return <BaseInput {...props} ref={ref} />
-})
+const Input = React.forwardRef(
+  (props: InputProps, ref: React.Ref<HTMLInputElement>) => {
+    const { size, ...rest } = props
+
+    let StyledInput
+
+    switch (size) {
+      case 'sm':
+        StyledInput = SmallInput
+        break
+      case 'lg':
+        StyledInput = LargeInput
+        break
+      default:
+        StyledInput = MediumInput
+        break
+    }
+
+    return <StyledInput {...rest} ref={ref} />
+  }
+)
 
 export { Input }
--- src/Input/styles.ts
+++ src/Input/styles.ts
@@ -4,7 +4,6 @@ import { rem } from 'polished'
 
 export const BaseInput = styled.input<InputProps>`
   display: inline-flex;
-  padding: ${`${rem('4px')} ${rem('11px')}`};
   border: ${rem('1px')} solid #d9d9d9;
   border-radius: ${rem('2px')};
   color: rgb(0, 0, 0, 0.6);
@@ -14,4 +13,16 @@ export const BaseInput = styled.input<InputProps>`
     border-color: ${({ theme }) => theme.colors.base};
     outline: none;
   }
+`
+
+export const SmallInput = styled(BaseInput)`
+  padding: ${`0 ${rem('7px')}`};
+`
+
+export const MediumInput = styled(BaseInput)`
+  padding: ${`${rem('4px')} ${rem('11px')}`};
+`
+
+export const LargeInput = styled(BaseInput)`
+  padding: ${`${rem('6.5px')} ${rem('11px')}`};
 `
