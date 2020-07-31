3d3c9cae85a7fcdcdc5a1f4c19ac010575ad9502
balta-z-r <zunigaruizb@gmail.com>
2020-07-31 12:49:20 +0800

    feat: updated snapshot containing all input types

--- src/Input/__tests__/Input.spec.tsx
+++ src/Input/__tests__/Input.spec.tsx
@@ -1,11 +1,60 @@
-import { Input } from '..'
+import { Input, PasswordInput, SearchInput } from '..'
 
 import React from 'react'
 import { render } from 'test/utils'
+import SettingsIcon from '@material-ui/icons/Settings'
 
 describe('Input', () => {
   it('renders correctly', () => {
-    const block = render(<Input />)
+    const block = render(
+      <>
+        <Input />
+        <div>
+          <Input size='lg' placeholder='Large Input' />
+          <br />
+          <br />
+          <Input placeholder='Medium Input' />
+          <br />
+          <br />
+          <Input size='sm' placeholder='Small Input' />
+        </div>
+        <div>
+          <Input addonBefore='http://' addonAfter='.com' />
+          <br />
+          <br />
+          <Input addonAfter={<SettingsIcon />} />
+        </div>
+        <div>
+          <Input prefix='￥' suffix='RMB' />
+          <br />
+          <br />
+          <Input prefix='￥' suffix='RMB' disabled />
+          <br />
+          <br />
+          <Input addonBefore='http://' suffix='.com' />
+        </div>
+
+        <div>
+          <Input placeholder='Borderless' bordered={false} />
+        </div>
+
+        <div>
+          <Input placeholder='Disabled' disabled />
+        </div>
+
+        <div>
+          <PasswordInput placeholder='Password' />
+        </div>
+        <div>
+          <SearchInput
+            placeholder='Search'
+            onSearch={(value) => console.log(value)}
+            enterButton
+          />
+          <br />
+        </div>
+      </>
+    )
 
     expect(block).toMatchSnapshot()
   })
