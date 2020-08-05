da115a2699bdee2f45445a3499a13a246da383df
balta-z-r <zunigaruizb@gmail.com>
2020-08-06 04:03:44 +0800

    feat: defaultValue, min, max and onChange properties

--- src/Slider/Slider.tsx
+++ src/Slider/Slider.tsx
@@ -7,20 +7,45 @@ export type SliderProps = {
   value?: number
   min?: number
   max?: number
+  onChange?: (value: number) => void
 }
 
 const Slider: FC<SliderProps> = (props) => {
+  const { onChange, ...rest } = props
+  const { defaultValue, min, max } = props
+
   const sliderRef = React.useRef<HTMLDivElement>(null)
   const thumbRef = React.useRef<HTMLDivElement>(null)
   const trackRef = React.useRef<HTMLDivElement>(null)
 
-  const getPercentage = (current: number, max: number) => (100 * current) / max
+  const getPercentage = (current: number, min: number, max: number) =>
+    (100 * (current - min)) / (max - min)
 
   const getLeft = (percentage: number) => `calc(${percentage}% - 5px)`
 
+  const getValue = (percentage: number, min: number, max: number) =>
+    ((max - min) / 100) * percentage + min
+
   const handleMouseMove = (event: MouseEvent) => {
+    updateValue(event.clientX)
+  }
+
+  const handleMouseUp = () => {
+    document.removeEventListener('mouseup', handleMouseUp)
+    document.removeEventListener('mousemove', handleMouseMove)
+  }
+
+  const handleMouseDown = (
+    event: React.MouseEvent<HTMLDivElement, MouseEvent>
+  ) => {
+    updateValue(event.clientX)
+    document.addEventListener('mousemove', handleMouseMove)
+    document.addEventListener('mouseup', handleMouseUp)
+  }
+
+  const updateValue = (x: number) => {
     if (!thumbRef.current || !sliderRef.current || !trackRef.current) return
-    let newX = event.clientX - sliderRef.current.getBoundingClientRect().left
+    let newX = x - sliderRef.current.getBoundingClientRect().left
 
     const end = sliderRef.current.offsetWidth - thumbRef.current.offsetWidth
 
@@ -30,29 +55,29 @@ const Slider: FC<SliderProps> = (props) => {
 
     if (newX > end) newX = end
 
-    const newPercentage = getPercentage(newX, end)
+    const newPercentage = getPercentage(newX, start, end)
 
     thumbRef.current.style.left = getLeft(newPercentage)
     trackRef.current.style.width = `${newPercentage}%`
+
+    if (onChange) onChange(getValue(newPercentage, min!, max!))
   }
 
-  const handleMouseUp = () => {
-    document.removeEventListener('mouseup', handleMouseUp)
-    document.removeEventListener('mousemove', handleMouseMove)
-  }
-
-  const handleMouseDown = () => {
-    document.addEventListener('mousemove', handleMouseMove)
-    document.addEventListener('mouseup', handleMouseUp)
-  }
+  const initialPercentage = getPercentage(defaultValue!, min!, max!)
 
   return (
-    <StyledSlider {...props} ref={sliderRef} onMouseDown={handleMouseDown}>
+    <StyledSlider {...rest} ref={sliderRef} onMouseDown={handleMouseDown}>
       <Rail />
-      <Track ref={trackRef} />
-      <Thumb ref={thumbRef} />
+      <Track ref={trackRef} style={{ width: `${initialPercentage}%` }} />
+      <Thumb ref={thumbRef} style={{ left: getLeft(initialPercentage) }} />
     </StyledSlider>
   )
+}
+
+Slider.defaultProps = {
+  min: 0,
+  max: 100,
+  defaultValue: 0
 }
 
 export { Slider }
--- src/Slider/styles.ts
+++ src/Slider/styles.ts
@@ -1,12 +1,13 @@
 import { SliderProps } from './Slider'
 
 import styled from 'styled-components'
+import { rem } from 'polished'
 
 export const StyledSlider = styled.div<SliderProps>`
   position: relative;
-  height: 4px;
+  height: ${rem('4px')};
   margin: 0;
-  padding: 4px 0;
+  padding: ${rem('4px')} 0;
   cursor: pointer;
 `
 
@@ -15,8 +16,8 @@ export const Rail = styled.div<SliderProps>`
   right: auto;
   left: 0;
   width: 100%;
-  height: 4px;
-  border-radius: 2px;
+  height: ${rem('4px')};
+  border-radius: ${rem('2px')};
   background-color: ${({ theme }) => theme.colors.gray4};
 `
 
@@ -24,16 +25,16 @@ export const Track = styled.div<SliderProps>`
   position: absolute;
   right: auto;
   left: 0;
-  height: 4px;
-  border-radius: 2px;
+  height: ${rem('4px')};
+  border-radius: ${rem('2px')};
   background-color: ${({ theme }) => theme.colors.base};
 `
 
 export const Thumb = styled.div<SliderProps>`
   position: relative;
-  top: -5px;
-  width: 14px;
-  height: 14px;
+  top: ${rem('-5px')};
+  width: ${rem('14px')};
+  height: ${rem('14px')};
   border-radius: 50%;
   background: ${({ theme }) => theme.colors.base};
 `
