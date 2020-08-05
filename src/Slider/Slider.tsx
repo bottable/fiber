05ae782a8139af104898a7451b0092f5e11a2303
balta-z-r <zunigaruizb@gmail.com>
2020-08-06 03:29:23 +0800

    feat: custom slider

--- src/Slider/Slider.tsx
+++ src/Slider/Slider.tsx
@@ -1,14 +1,58 @@
-import { StyledSlider } from './styles'
+import { StyledSlider, Thumb, Track, Rail } from './styles'
 
 import React, { FC } from 'react'
 
 export type SliderProps = {
   defaultValue?: number
   value?: number
+  min?: number
+  max?: number
 }
 
 const Slider: FC<SliderProps> = (props) => {
-  return <StyledSlider type='range' {...props} />
+  const sliderRef = React.useRef<HTMLDivElement>(null)
+  const thumbRef = React.useRef<HTMLDivElement>(null)
+  const trackRef = React.useRef<HTMLDivElement>(null)
+
+  const getPercentage = (current: number, max: number) => (100 * current) / max
+
+  const getLeft = (percentage: number) => `calc(${percentage}% - 5px)`
+
+  const handleMouseMove = (event: MouseEvent) => {
+    if (!thumbRef.current || !sliderRef.current || !trackRef.current) return
+    let newX = event.clientX - sliderRef.current.getBoundingClientRect().left
+
+    const end = sliderRef.current.offsetWidth - thumbRef.current.offsetWidth
+
+    const start = 0
+
+    if (newX < start) newX = 0
+
+    if (newX > end) newX = end
+
+    const newPercentage = getPercentage(newX, end)
+
+    thumbRef.current.style.left = getLeft(newPercentage)
+    trackRef.current.style.width = `${newPercentage}%`
+  }
+
+  const handleMouseUp = () => {
+    document.removeEventListener('mouseup', handleMouseUp)
+    document.removeEventListener('mousemove', handleMouseMove)
+  }
+
+  const handleMouseDown = () => {
+    document.addEventListener('mousemove', handleMouseMove)
+    document.addEventListener('mouseup', handleMouseUp)
+  }
+
+  return (
+    <StyledSlider {...props} ref={sliderRef} onMouseDown={handleMouseDown}>
+      <Rail />
+      <Track ref={trackRef} />
+      <Thumb ref={thumbRef} />
+    </StyledSlider>
+  )
 }
 
 export { Slider }
--- src/Slider/styles.ts
+++ src/Slider/styles.ts
@@ -2,7 +2,38 @@ import { SliderProps } from './Slider'
 
 import styled from 'styled-components'
 
-export const StyledSlider = styled.input<SliderProps>`
-  width: 100%;
+export const StyledSlider = styled.div<SliderProps>`
+  position: relative;
+  height: 4px;
+  margin: 0;
+  padding: 4px 0;
   cursor: pointer;
+`
+
+export const Rail = styled.div<SliderProps>`
+  position: absolute;
+  right: auto;
+  left: 0;
+  width: 100%;
+  height: 4px;
+  border-radius: 2px;
+  background-color: ${({ theme }) => theme.colors.gray4};
+`
+
+export const Track = styled.div<SliderProps>`
+  position: absolute;
+  right: auto;
+  left: 0;
+  height: 4px;
+  border-radius: 2px;
+  background-color: ${({ theme }) => theme.colors.base};
+`
+
+export const Thumb = styled.div<SliderProps>`
+  position: relative;
+  top: -5px;
+  width: 14px;
+  height: 14px;
+  border-radius: 50%;
+  background: ${({ theme }) => theme.colors.base};
 `
