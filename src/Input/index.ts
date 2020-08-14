0b0da20edd58012f2c341d9f13c69bf51fa4437d
balta-z-r <zunigaruizb@gmail.com>
2020-08-14 10:24:17 +0800

    fix: removed circular dependencies

--- src/Input/index.ts
+++ src/Input/index.ts
@@ -1,1 +1,8 @@
-export * from './Input'
+import { Input } from './Input'
+import Password from './Password'
+import Search from './Search'
+
+Input.Password = Password
+Input.Search = Search
+
+export { Input }
