89f8b5c001bd14fe04fc8bb838206240e28f4517
balta-z-r <zunigaruizb@gmail.com>
2020-08-17 08:48:56 +0800

    feat: basic styling

--- src/Checkbox/Checkbox.tsx
+++ src/Checkbox/Checkbox.tsx
@@ -1,13 +1,66 @@
-import { StyledCheckbox } from './styles'
+import {
+  Wrapper,
+  CheckboxContainer,
+  LabelContainer,
+  CheckboxInput,
+  StyledCheckbox
+} from './styles'
 
-import React, { FC } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 
 export type CheckboxProps = {
-  // custom props here
+  checked?: boolean
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void
+  style?: object
+  disabled?: boolean
 }
 
-const Checkbox: FC<CheckboxProps> = ({ ...props }) => {
-  return <StyledCheckbox {...props} type='checkbox' />
-}
+const Checkbox: FC<CheckboxProps> = React.forwardRef<
+  HTMLInputElement,
+  CheckboxProps
+>(
+  (
+    { children, checked: checkedProps, onChange, style, disabled, ...props },
+    ref
+  ) => {
+    const [checked, setChecked] = useState<boolean>(Boolean(checkedProps))
+
+    useEffect(() => {
+      if (typeof checkedProps === 'boolean') setChecked(checkedProps)
+    }, [checkedProps])
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      if (typeof checkedProps !== 'boolean') setChecked(e.target.checked)
+      if (onChange) {
+        onChange(e)
+      }
+    }
+
+    const CheckboxNode = (
+      <CheckboxContainer>
+        <CheckboxInput
+          type='Checkbox'
+          checked={checked}
+          onChange={handleChange}
+          disabled={disabled}
+          {...props}
+          ref={ref}
+        />
+        <StyledCheckbox checked={checked} disabled={disabled} />
+      </CheckboxContainer>
+    )
+
+    const labelNode = React.Children.count(children) ? (
+      <LabelContainer>{children}</LabelContainer>
+    ) : null
+
+    return (
+      <Wrapper style={style} disabled={disabled}>
+        {CheckboxNode}
+        {labelNode}
+      </Wrapper>
+    )
+  }
+)
 
 export { Checkbox }
--- src/Checkbox/styles.ts
+++ src/Checkbox/styles.ts
@@ -2,4 +2,46 @@ import { CheckboxProps } from './Checkbox'
 
 import styled from 'styled-components'
 
-export const StyledCheckbox = styled.input<CheckboxProps>``
+export const StyledCheckbox = styled.span<CheckboxProps>`
+  display: block;
+  position: relative;
+  top: 0;
+  left: 0;
+  width: ${({ theme }) => theme.space[3]};
+  height: ${({ theme }) => theme.space[3]};
+  border: ${({ theme }) => theme.border.md};
+  border-radius: ${({ theme }) => theme.radii.md};
+  border-color: ${({ checked, theme }) =>
+    checked ? theme.colors.base : theme.colors.gray4};
+  background-color: ${({ checked, theme }) =>
+    checked ? theme.colors.base : null};
+
+  &:hover {
+    border-color: ${({ theme }) => theme.colors.base};
+  }
+
+  &::after {
+    content: ' ';
+    display: table;
+    position: absolute;
+    top: 50%;
+    left: 22%;
+    width: 5.71428571px;
+    height: 9.14285714px;
+    border: ${({ theme }) => theme.border.lg} #fff;
+    border-top: 0;
+    border-left: 0;
+    opacity: ${({ checked }) => (checked ? 1 : 0)};
+    transform: rotate(45deg) scale(1) translate(-50%, -50%);
+    ${({ theme }) => theme.transition}
+  }
+
+  ${({ theme }) => theme.transition}
+`
+
+export {
+  Wrapper,
+  LabelContainer,
+  RadioContainer as CheckboxContainer,
+  RadioInput as CheckboxInput
+} from '../Radio/styles'
