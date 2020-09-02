752337e1153c19a0f5b885700e9c2d3e9c36e4b9
balta-z-r <zunigaruizb@gmail.com>
2020-09-02 09:02:48 +0800

    feat: table head and table body

--- src/Table/Table.tsx
+++ src/Table/Table.tsx
@@ -1,13 +1,68 @@
-import { StyledTable } from './styles'
+import {
+  Wrapper,
+  ContentContainer,
+  StyledTable,
+  TableHead,
+  TableBody,
+  TableCell
+} from './styles'
 
 import React, { FC } from 'react'
 
-export type TableProps = {
-  // custom props here
+type columnItem = {
+  title: string
+  key: string
+  dataIndex?: string
+  render?: () => React.ReactNode
 }
 
-const Table: FC<TableProps> = ({ children, ...props }) => {
-  return <StyledTable {...props}>{children}</StyledTable>
+export type TableProps = {
+  columns?: columnItem[]
+  dataSource?: object[]
+}
+
+const Table: FC<TableProps> = ({ columns, dataSource, ...props }) => {
+  let tableHeadNode
+  let tableBodyNode
+
+  if (columns) {
+    tableHeadNode = (
+      <TableHead>
+        <tr>
+          {columns.map(({ title }, idx) => {
+            return <TableCell key={idx}>{title}</TableCell>
+          })}
+        </tr>
+      </TableHead>
+    )
+  }
+
+  if (dataSource && columns) {
+    tableBodyNode = (
+      <TableBody>
+        {dataSource.map((dataItem, idx) => {
+          return (
+            <tr key={idx}>
+              {columns.map(({ key }, i) => {
+                return <TableCell key={i}>{dataItem[key]}</TableCell>
+              })}
+            </tr>
+          )
+        })}
+      </TableBody>
+    )
+  }
+
+  return (
+    <Wrapper {...props}>
+      <ContentContainer>
+        <StyledTable>
+          {tableHeadNode}
+          {tableBodyNode}
+        </StyledTable>
+      </ContentContainer>
+    </Wrapper>
+  )
 }
 
 export { Table }
--- src/Table/styles.ts
+++ src/Table/styles.ts
@@ -2,4 +2,14 @@ import { TableProps } from './Table'
 
 import styled from 'styled-components'
 
-export const StyledTable = styled.div<TableProps>``
+export const Wrapper = styled.div<TableProps>``
+
+export const ContentContainer = styled.div<TableProps>``
+
+export const StyledTable = styled.table<TableProps>``
+
+export const TableHead = styled.thead<TableProps>``
+
+export const TableBody = styled.tbody<TableProps>``
+
+export const TableCell = styled.th<TableProps>``
