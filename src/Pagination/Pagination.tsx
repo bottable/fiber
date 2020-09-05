f9b3bf637e0b631f0fcc6ad47ccde33b57972f65
balta-z-r <zunigaruizb@gmail.com>
2020-09-05 16:12:54 +0800

    feat: pagination basic styling and functionality

--- src/Pagination/Pagination.tsx
+++ src/Pagination/Pagination.tsx
@@ -1,13 +1,72 @@
-import { StyledPagination } from './styles'
+import { StyledPagination, PaginationItem, Button } from './styles'
 
-import React, { FC } from 'react'
+import NavigateBeforeIcon from '@material-ui/icons/NavigateBefore'
+import NavigateNextIcon from '@material-ui/icons/NavigateNext'
+import React, { FC, useState } from 'react'
 
-export type PaginationProps = {
-  // custom props here
+export type ButtonProps = {
+  selected: boolean
 }
 
-const Pagination: FC<PaginationProps> = ({ children, ...props }) => {
-  return <StyledPagination {...props}>{children}</StyledPagination>
+export type PaginationProps = {
+  defaultCurrent?: number
+  defaultPageSize?: number
+  total?: number
+}
+
+const Pagination: FC<PaginationProps> = ({
+  defaultCurrent,
+  defaultPageSize,
+  total
+}) => {
+  const [current, setCurrent] = useState<number>(defaultCurrent || 0)
+
+  const n = Math.ceil(total! / defaultPageSize!)
+
+  const pageNumbersNode: React.ReactElement[] = []
+
+  for (let i = 1; i <= n; i++) {
+    pageNumbersNode.push(
+      <PaginationItem
+        onClick={() => {
+          setCurrent(i)
+        }}
+      >
+        <Button selected={current === i}>{i}</Button>
+      </PaginationItem>
+    )
+  }
+
+  return (
+    <StyledPagination>
+      <PaginationItem>
+        <Button
+          selected={false}
+          icon={<NavigateBeforeIcon />}
+          onClick={() => {
+            setCurrent((prevCurrent) => prevCurrent - 1)
+          }}
+          disabled={current === 1}
+        />
+      </PaginationItem>
+      {pageNumbersNode}
+      <PaginationItem>
+        <Button
+          selected={false}
+          icon={<NavigateNextIcon />}
+          onClick={() => {
+            setCurrent((prevCurrent) => prevCurrent + 1)
+          }}
+          disabled={current === n}
+        />
+      </PaginationItem>
+    </StyledPagination>
+  )
+}
+
+Pagination.defaultProps = {
+  defaultPageSize: 10,
+  total: 0
 }
 
 export { Pagination }
--- src/Pagination/styles.ts
+++ src/Pagination/styles.ts
@@ -1,5 +1,25 @@
-import { PaginationProps } from './Pagination'
+import { Button as BaseButton } from '../Button'
+
+import { ButtonProps } from './Pagination'
 
 import styled from 'styled-components'
 
-export const StyledPagination = styled.div<PaginationProps>``
+export const StyledPagination = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+`
+
+export const PaginationItem = styled.li`
+  display: inline-block;
+  list-style: none;
+  &:not(:last-child) {
+    margin-right: ${({ theme }) => theme.margins.xs};
+  }
+`
+
+export const Button = styled(BaseButton)<ButtonProps>`
+  border-color: ${({ selected, theme }) =>
+    selected ? theme.colors.base : null};
+  color: ${({ selected, theme }) => (selected ? theme.colors.base : null)};
+`
